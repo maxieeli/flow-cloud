dd2b36e7d2909e46268f7630fe89a410cb27afb3
maxiee <owenlideveloper@gmail.com>
2024-05-10 07:23:41 +0800

    feat: update lib client-api wasm retry

---
+++ lib/client-api/src/wasm/retry.rs
@@ -0,0 +1,15 @@
+use crate::ws::{ConnectInfo, CurrentConnInfo, StateNotify, WSError};
+use client_websocket::{connect_async, WebSocketStream};
+use reqwest::header::HeaderMap;
+use std::sync::Weak;
+
+pub async fn retry_connect(
+    url: String,
+    info: ConnectInfo,
+    _state_notify: Weak<StateNotify>,
+    _current_addr: Weak<CurrentConnInfo>,
+) -> Result<WebSocketStream, WSError> {
+    let headers: HeaderMap = info.into();
+    let stream = connect_async(url, headers).await?;
+    Ok(stream)
+}
