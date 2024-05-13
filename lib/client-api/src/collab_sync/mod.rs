5c643a52def33877627b623ae35fc3717b4a9b06
maxiee <owenlideveloper@gmail.com>
2024-05-14 07:17:41 +0800

    feat: update client-api collab-sync mod

---
+++ lib/client-api/src/collab_sync/mod.rs
@@ -0,0 +1,16 @@
+mod channel;
+mod error;
+mod plugin;
+mod sink;
+mod sink_config;
+mod sink_queue;
+mod sync_control;
+
+pub use channel::*;
+pub use error::*;
+pub use plugin::*;
+pub use sink::*;
+pub use sink_config::*;
+pub use sync_control::*;
+
+pub use realtime_entity::collab_msg;
