e393fd4274fc903f932d9006eee49b43db6aa41c
maxiee <owenlideveloper@gmail.com>
2024-05-18 07:39:21 +0800

    update client-api plugin Sync-Plugin

--- lib/client-api/src/collab_sync/plugin.rs
+++ lib/client-api/src/collab_sync/plugin.rs
@@ -20,6 +20,101 @@ use crate::collab_sync::sink_config::SinkConfig;
 use crate::ws::{ConnectState, WSConnectStateReceiver};
 use yrs::updates::encoder::Encode;
 
+pub struct SyncPlugin<Sink, Stream, C> {
+    object: SyncObject,
+    sync_queue: Arc<SyncControl<Sink, Stream>>,
+    // Used to keep the lifetime of the channel
+    #[allow(dead_code)]
+    channel: Option<Arc<C>>,
+}
+
+impl<Sink, Stream, C> Drop for SyncPlugin<Sink, Stream, C> {
+    fn drop(&mut self) {
+        trace!("Drop sync plugin: {}", self.object.object_id);
+    }
+}
+
+impl<E, Sink, Stream, C> SyncPlugin<Sink, Stream, C>
+where
+    E: Into<anyhow::Error> + Send + Sync + 'static,
+    Sink: SinkExt<Vec<ClientCollabMessage>, Error = E> + Send + Sync + Unpin + 'static,
+    Stream: StreamExt<Item = Result<ServerCollabMessage, E>> + Send + Sync + Unpin + 'static,
+    C: Send + Sync + 'static,
+{
+    #[allow(clippy::too_many_arguments)]
+    pub fn new(
+        origin: CollabOrigin,
+        object: SyncObject,
+        collab: Weak<MutexCollab>,
+        sink: Sink,
+        sink_config: SinkConfig,
+        stream: Stream,
+        channel: Option<Arc<C>>,
+        pause: bool,
+        mut ws_connect_state: WSConnectStateReceiver,
+    ) -> Self {
+        let weak_local_collab = collab.clone();
+        let sync_queue = SyncControl::new(
+            object.clone(),
+            origin,
+            sink,
+            sink_config,
+            stream,
+            collab.clone(),
+            pause,
+        );
+        let mut sync_state_stream = WatchStream::new(sync_queue.subscribe_sync_state());
+        af_spawn(async move {
+            while let Some(new_state) = sync_state_stream.next().await {
+                if let Some(local_collab) = weak_local_collab.upgrade() {
+                    if let Some(local_collab) = local_collab.try_lock() {
+                        local_collab.set_sync_state(new_state);
+                    }
+                }
+            }
+        });
+        let sync_queue = Arc::new(sync_queue);
+        let weak_local_collab = collab;
+        let weak_sync_queue = Arc::downgrade(&sync_queue);
+        af_spawn(async move {
+            while let Ok(connect_state) = ws_connect_state.recv().await {
+                match connect_state {
+                    Connect::Connected => {
+                        // if the websocket is connected, initialize a new init sync
+                        if let (Some(local_collab), Some(sync_queue)) = (weak_local_collab.upgrade(), weak_sync_queue.upgrade())
+                        {
+                            if let Some(local_collab) = local_collab.try_lock() {
+                                sync_queue.resume();
+                                sync_queue.init_sync(&local_collab);
+                            }
+                        } else {
+                            break;
+                        }
+                    },
+                    ConnectState::Unauthorized | ConnectState::Lost => {
+                        if let Some(sync_queue) = weak_sync_queue.upgrade() {
+                            sync_queue.pause();
+                        } else {
+                            break;
+                        }
+                    },
+                    _ => {},
+                }
+            }
+        });
+        Self {
+            sync_queue,
+            object,
+            channel,
+        }
+    }
+
+    pub fn subscribe_sync_state(&self) -> WatchStream<SyncState> {
+        let rx = self.sync_queue.subscribe_sync_state();
+        WatchStream::new(rx)
+    }
+}
+
 
 #[derive(Clone, Debug)]
 pub struct SyncObject {
