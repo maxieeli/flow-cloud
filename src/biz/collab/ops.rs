a9cae222573ed8efe1c8b4fdd1517c657eea5791
maxiee <owenlideveloper@gmail.com>
2024-06-10 14:43:13 +0800

    feat: update biz ops

---
+++ src/biz/collab/ops.rs
@@ -0,0 +1,156 @@
+use anyhow::Context;
+use std::ops::DerefMut;
+use app_error::AppError;
+use database_entity::dto::{
+    AFCollabMember, CollabMemberIdentify, DeleteCollabParams,
+    InsertCollabMemberParams, QueryCollabMembers, UpdateCollabMemberParams,
+};
+use crate::biz::collab::access_control::CollabAccessControl;
+use sqlx::{types::Uuid, PgPool};
+use tracing::{event, trace};
+use validator::Validate;
+
+pub async fn delete_collab(
+    pg_pool: &PgPool,
+    _user_uuid: &Uuid,
+    params: &DeleteCollabParams,
+) -> Result<(), AppError> {
+    params.validate()?;
+    database::collab::delete_collab(pg_pool, &params.object_id).await?;
+    Ok(())
+}
+
+/// Create a new collab member
+/// If the collab member already exists, return [AppError::RecordAlreadyExists]
+/// If the collab member does not exist, create a new one
+pub async fn create_collab_member(
+    pg_pool: &PgPool,
+    params: &InsertCollabMemberParams,
+    collab_access_control: &impl CollabAccessControl,
+) -> Result<(), AppError> {
+    params.validate()?;
+    let mut transaction = pg_pool
+        .begin()
+        .await
+        .context("acquire transaction to insert collab member")?;
+    if !database::collab::is_collab_exists(&params.object_id, transaction.deref_mut()).await? {
+        return Err(AppError::RecordNotFound(format!(
+            "Fail to insert collab member. The Collab with object_id {} does not exist",
+            params.object_id
+        )));
+    }
+    if database::collab::is_collab_member_exists(
+        params.uid,
+        &params.object_id,
+        transaction.deref_mut(),
+    )
+    .await?
+    {
+        return Err(AppError::RecordAlreadyExists(format!(
+            "Collab member with uid {} and object_id {} already exists",
+            params.uid, params.object_id
+        )));
+    }
+    trace!("Inserting collab member: {:?}", params);
+    database::collab::insert_collab_member(
+        params.uid,
+        &params.object_id,
+        &params.access_level,
+        &mut transaction,
+    )
+    .await?;
+
+    collab_access_control
+        .update_access_level_policy(&params.uid, &params.object_id, params.access_level)
+        .await?;
+    transaction
+        .commit()
+        .await
+        .context("fail to commit the transaction to insert collab member")?;
+    Ok(())
+}
+
+pub async fn upsert_collab_member(
+    pg_pool: &PgPool,
+    _user_uuid: &Uuid,
+    params: &UpdateCollabMemberParams,
+    collab_access_control: &impl CollabAccessControl,
+) -> Result<(), AppError> {
+    params.validate()?;
+    let mut transaction = pg_pool
+        .begin()
+        .await
+        .context("acquire transaction to upsert collab member")?;
+
+    if !database::collab::is_collab_exists(&params.object_id, transaction.deref_mut()).await? {
+        return Err(AppError::RecordNotFound(format!(
+            "Fail to upsert collab member. The Collab with object_id {} does not exist",
+            params.object_id
+        )));
+    }
+        
+    collab_access_control
+        .update_access_level_policy(&params.uid, &params.object_id, params.access_level)
+        .await?;
+
+    database::collab::insert_collab_member(
+        params.uid,
+        &params.object_id,
+        &params.access_level,
+        &mut transaction,
+    )
+    .await?;
+        
+    transaction
+        .commit()
+        .await
+        .context("fail to commit the transaction to upsert collab member")?;
+    Ok(())
+}
+
+pub async fn get_collab_member(
+    pg_pool: &PgPool,
+    params: &CollabMemberIdentify,
+) -> Result<AFCollabMember, AppError> {
+    params.validate()?;
+    let collab_member = database::collab::select_collab_member(&params.uid, &params.object_id, pg_pool).await?;
+    Ok(collab_member)
+}
+
+pub async fn delete_collab_member(
+    pg_pool: &PgPool,
+    params: &CollabMemberIdentify,
+    collab_access_control: &impl CollabAccessControl,
+) -> Result<(), AppError> {
+    params.validate()?;
+    let mut transaction = pg_pool
+        .begin()
+        .await
+        .context("acquire transaction to remove collab member")?;
+    event!(
+        tracing::Level::DEBUG,
+        "Deleting member:{} from {}",
+        params.uid,
+        params.object_id
+    );
+    database::collab::delete_collab_member(params.uid, &params.object_id, &mut transaction).await?;
+  
+    collab_access_control
+        .remove_access_level(&params.uid, &params.object_id)
+        .await?;
+  
+    transaction
+        .commit()
+        .await
+        .context("fail to commit the transaction to remove collab member")?;
+    Ok(())
+}
+
+pub async fn get_collab_member_list(
+    pg_pool: &PgPool,
+    params: &QueryCollabMembers,
+) -> Result<Vec<AFCollabMember>, AppError> {
+    params.validate()?;
+    let collab_member = database::collab::select_collab_members(&params.object_id, pg_pool).await?;
+    Ok(collab_member)
+}
