8eb693cc318f0254012d224585d03d5be4cac399
maxiee <owenlideveloper@gmail.com>
2024-06-19 07:15:58 +0800

    feat: update biz pg-listener

--- src/biz/pg_listener.rs
+++ src/biz/pg_listener.rs
@@ -54,3 +54,37 @@ impl PgListeners {
         rx
     }
 }
+
+pub struct PostgresDBListener<T: Clone> {
+    notify: broadcast::Sender<T>,
+}
+
+impl<T> PostgresDBListener<T>
+where
+    T: Clone + DeserializeOwned + Send + 'static,
+{
+    pub async fn new(pg_pool: &PgPool, channel: &str) -> Result<Self, Error> {
+        let mut listener = PgListener::connect_with(pg_pool).await?;
+        listener.listen(channel).await?;
+        let (tx, _) = broadcast::channel(1000);
+        let notify = tx.clone();
+        tokio::spawn(async move {
+            while let Ok(notification) = listener.recv().await {
+                trace!("Received notification: {}", notification.payload());
+                match serde_json::from_str::<T>(notification.payload()) {
+                    Ok(change) => {
+                        let _ = tx.send(change);
+                    },
+                    Err(err) => {
+                        error!(
+                            "Failed to deserialize change: {:?}, payload: {}",
+                            err,
+                            notification.payload()
+                        );
+                    },
+                }
+            }
+        });
+        Ok(Self { notify })
+    }
+}
