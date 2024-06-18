fa27848c91608fdaf69a6c29699542476d2798f8
maxiee <owenlideveloper@gmail.com>
2024-06-19 07:37:51 +0800

    feat: update biz user

--- src/biz/user.rs
+++ src/biz/user.rs
@@ -107,5 +107,89 @@ where
     Ok(())
 }
 
+pub async fn get_profile(pg_pool: &PgPool, uuid: &Uuid) -> Result<AFUserProfile, AppError> {
+    let row = select_user_profile(pg_pool, uuid)
+        .await
+        .ok_or(AppError::RecordNotFound(format!(
+            "Can't find the user profile for user: {}",
+            uuid
+        )))?;
+    let profile = AFUserProfile::try_from(row)?;
+    Ok(profile)
+}
 
+#[instrument(level = "debug", skip(pg_pool), err)]
+pub async fn get_user_workspace_info(
+    pg_pool: &PgPool,
+    uuid: &Uuid,
+) -> Result<AFUserWorkspaceInfo, AppError> {
+    let mut txn = pg_pool
+        .begin()
+        .await
+        .context("failed to acquire the transaction to query the user workspace info")?;
+    let row = select_user_profile(txn.deref_mut(), uuid)
+        .await?
+        .ok_or(AppError::RecordNotFound(format!(
+            "Can't find the user profile for {}",
+            uuid
+        )))?;
+    let visiting_workspace = AFWorkspace::try_from(
+        select_workspace(txn.deref_mut(), &row.latest_workspace_id.unwrap()).await?,
+    )?;
+        
+    // Get the user profile
+    let user_profile = AFUserProfile::try_from(row)?;
 
+    // Get all workspaces that the user can access to
+    let workspaces = select_user_workspace(txn.deref_mut(), uuid)
+        .await?
+        .into_iter()
+        .flat_map(|row| AFWorkspace::try_from(row).ok())
+        .collect::<Vec<AFWorkspace>>();
+    txn
+        .commit()
+        .await
+        .context("failed to commit the transaction to get user workspace info")?;
+    Ok(AFUserWorkspaceInfo {
+        user_profile,
+        visiting_workspace,
+        workspaces,
+    })
+}
+
+pub async fn update_user(
+    pg_pool: &PgPool,
+    user_uuid: Uuid,
+    params: UpdateUserParams,
+) -> Result<(), AppResponseError> {
+    let metadata = params.metadata.map(|m| json!(m.into_inner()));
+    Ok(database::user::update_user(pg_pool, &user_uuid, params.name, params.email, metadata).await?)
+}
+
+fn name_from_user_metadata(value: &serde_json::Value) -> String {
+    value
+        .get("name")
+        .or(value.get("full_name"))
+        .or(value.get("nickname"))
+        .and_then(serde_json::Value::as_str)
+        .map(str::to_string)
+        .unwrap_or_default()
+}
+
+pub type UserListener = crate::biz::pg_listener::PostgresDBListener<AFUserNotification>;
+#[derive(Debug, Clone, Eq, PartialEq, Hash)]
+pub struct RealtimeUserImpl {
+    pub uid: i64,
+    pub device_id: String,
+    pub timestamp: i64,
+}
+
+impl RealtimeUserImpl {
+    pub fn new(uid: i64, device_id: String) -> Self {
+        Self {
+            uid,
+            device_id,
+            timestamp: chrono::Utc::now().timestamp(),
+        }
+    }
+}
