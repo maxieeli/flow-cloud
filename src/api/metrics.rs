94e24d521bfc455771fb3a916c41176191e5803c
maxiee <owenlideveloper@gmail.com>
2024-06-21 07:18:58 +0800

    feat: update api metrics

--- src/api/metrics.rs
+++ src/api/metrics.rs
@@ -27,3 +27,75 @@ async fn metrics_handler(reg: web::Data<Arc<Registry>>) -> Result<HttpResponse> 
     )
 }
 
+#[derive(Clone, Debug, Hash, PartialEq, Eq, EncodeLabelSet)]
+pub struct PathLabel {
+    pub path: String,
+}
+
+#[derive(Clone, Debug, Hash, PartialEq, Eq, EncodeLabelSet)]
+pub struct ResultLabel {
+    pub path: String,
+    pub status_code: u16,
+}
+
+// Metrics contains list of metrics that are collected by the application.
+// Metric types: https://prometheus.io/docs/concepts/metric_types
+// Application handlers should call the corresponding methods to update the metrics.
+#[derive(Clone)]
+pub struct RequestMetrics {
+    requests_count: Family<PathLabel, Counter>,
+    requests_latency: Family<PathLabel, CounterWithExemplar<TraceLabel>>,
+    requests_result: Family<ResultLabel, CounterWithExemplar<TraceLabel>>,
+}
+
+#[derive(Clone, Hash, PartialEq, Eq, EncodeLabelSet, Debug, Default)]
+pub struct TraceLabel {
+    pub trace_id: String,
+}
+
+impl RequestMetrics {
+    fn init() -> Self {
+        Self {
+            requests_count: Family::default(),
+            requests_latency: Family::default(),
+            requests_result: Family::default(),
+        }
+    }
+
+    pub fn register(registry: &mut Registry) -> Self {
+        let af_metrics = Self::init();
+        let af_registry = registry.sub_registry_with_prefix("appflow_cloud");
+        af_registry.register(
+            "requests_count",
+            "number of requests",
+            af_metrics.requests_count.clone(),
+        );
+        af_registry.register(
+            "requests_latency",
+            "request response time",
+            af_metrics.requests_latency.clone(),
+        );
+        af_registry.register(
+            "requests_result",
+            "status code of response",
+            af_metrics.requests_result.clone(),
+        );
+        af_metrics
+    }
+
+    // app services/middleware should call this method to increase the request count for the path
+    pub fn record_request(&self, trace_id: Option<String>, path: String, ms: u64, status_code: u16) {
+        self
+            .requests_count
+            .get_or_create(&PathLabel { path: path.clone() })
+            .inc();
+        self
+            .requests_latency
+            .get_or_create(&PathLabel { path: path.clone() })
+            .inc_by(ms, trace_id.clone().map(|s| TraceLabel { trace_id: s }));
+        self
+            .requests_result
+            .get_or_create(&ResultLabel { path, status_code })
+            .inc_by(1, trace_id.clone().map(|s| TraceLabel { trace_id: s }));
+    }
+}
