b261b9d6f64973d0fe6c7d1acb9e9817460847e6
maxiee <owenlideveloper@gmail.com>
2024-06-23 17:20:33 +0800

    feat: update application

--- src/application.rs
+++ src/application.rs
@@ -143,3 +143,244 @@ pub async fn run(
     };
     Ok(server.run())
 }
+
+fn get_certificate_and_server_key(config: &Config) -> Option<(Secret<String>, Secret<String>)> {
+    if config.application.use_tls {
+        Some(create_self_signed_certificate().unwrap())
+    } else {
+        None
+    }
+}
+  
+pub async fn init_state(config: &Config, rt_cmd_tx: RTCommandSender) -> Result<AppState, Error> {
+    // Print the feature flags
+    let metrics = AppMetrics::new();
+    // Postgres
+    info!("Preparing to run database migrations...");
+    let pg_pool = get_connection_pool(&config.db_settings).await?;
+    // migrate(&pg_pool).await?;
+    // Bucket storage
+    info!("Setting up S3 bucket...");
+    let s3_bucket = get_aws_s3_bucket(&config.s3).await?;
+    let bucket_storage = Arc::new(S3BucketStorage::from_s3_bucket(s3_bucket, pg_pool.clone()));
+    // Gotrue
+    info!("Connecting to GoTrue...");
+    let gotrue_client = get_gotrue_client(&config.gotrue).await?;
+    let gotrue_admin = setup_admin_account(&gotrue_client, &pg_pool, &config.gotrue).await?;
+    // Redis
+    info!("Connecting to Redis...");
+    let redis_client = get_redis_client(config.redis_uri.expose_secret()).await?;
+    #[cfg(feature = "ai_enable")]
+    let appflowy_ai_client = appflowy_ai::client::AppFlowyAIClient::new(config.appflowy_ai.url.expose_secret());
+    // Pg listeners
+    info!("Setting up Pg listeners...");
+    let pg_listeners = Arc::new(PgListeners::new(&pg_pool).await?);
+    let collab_member_listener = pg_listeners.subscribe_collab_member_change();
+    let workspace_member_listener = pg_listeners.subscribe_workspace_member_change();
+    info!(
+        "Setting up access controls, is_enable: {}",
+        enable_access_control()
+    );
+    let access_control = AccessControl::new(pg_pool.clone(), metrics.access_control_metrics.clone()).await?;
+    spawn_listen_on_workspace_member_change(workspace_member_listener, access_control.clone());
+    spawn_listen_on_collab_member_change(
+        pg_pool.clone(),
+        collab_member_listener,
+        access_control.clone(),
+    );
+    let user_cache = UserCache::new(pg_pool.clone()).await;
+    let collab_access_control = access_control.new_collab_access_control();
+    let workspace_access_control = access_control.new_workspace_access_control();
+    let collab_cache = CollabCache::new(redis_client.clone(), pg_pool.clone());
+    let collab_storage_access_control = CollabStorageAccessControlImpl {
+        collab_access_control: collab_access_control.clone().into(),
+        workspace_access_control: workspace_access_control.clone().into(),
+        cache: collab_cache.clone(),
+    };
+    let snapshot_control = SnapshotControl::new(
+        redis_client.clone(),
+        pg_pool.clone(),
+        metrics.collab_metrics.clone(),
+    )
+    .await;
+    let collab_storage = Arc::new(CollabStorageImpl::new(
+        collab_cache.clone(),
+        collab_storage_access_control,
+        snapshot_control,
+        rt_cmd_tx,
+    ));
+  
+    info!("Application state initialized");
+    Ok(AppState {
+        pg_pool,
+        config: Arc::new(config.clone()),
+        user_cache,
+        id_gen: Arc::new(RwLock::new(Snowflake::new(1))),
+        gotrue_client,
+        redis_client,
+        collab_cache,
+        collab_access_control_storage: collab_storage,
+        collab_access_control,
+        workspace_access_control,
+        bucket_storage,
+        pg_listeners,
+        access_control,
+        metrics,
+        gotrue_admin,
+        #[cfg(feature = "ai_enable")]
+        appflowy_ai_client,
+    })
+}
+  
+async fn setup_admin_account(
+    gotrue_client: &gotrue::api::Client,
+    pg_pool: &PgPool,
+    gotrue_setting: &GoTrueSetting,
+) -> Result<GoTrueAdmin, Error> {
+    let admin_email = gotrue_setting.admin_email.as_str();
+    let password = gotrue_setting.admin_password.expose_secret();
+    let gotrue_admin = GoTrueAdmin::new(admin_email.to_owned(), password.to_owned());
+  
+    let res_resp = gotrue_client.sign_up(admin_email, password, None).await;
+    match res_resp {
+        Err(err) => {
+            if let app_error::gotrue::GoTrueError::Internal(err) = err {
+                match (err.code, err.msg.as_str()) {
+                    (400, "User already registered") => {
+                        info!("Admin user already registered");
+                        Ok(gotrue_admin)
+                    },
+                    _ => Err(err.into()),
+                }
+            } else {
+                Err(err.into())
+            }
+        },
+        Ok(resp) => {
+            let admin_user = {
+                match resp {
+                    gotrue_entity::dto::SignUpResponse::Authenticated(resp) => resp.user,
+                    gotrue_entity::dto::SignUpResponse::NotAuthenticated(user) => user,
+                }
+            };
+            match admin_user.role.as_str() {
+                "supabase_admin" => {
+                    info!("Admin user already created and set role to supabase_admin");
+                    Ok(gotrue_admin)
+                },
+                _ => {
+                    let user_id = admin_user.id.parse::<uuid::Uuid>()?;
+                    let result = sqlx::query!(
+                        r#"
+                        UPDATE auth.users
+                        SET role = 'supabase_admin', email_confirmed_at = NOW()
+                        WHERE id = $1
+                        "#,
+                        user_id,
+                    )
+                    .execute(pg_pool)
+                    .await
+                    .context("failed to update the admin user")?;
+                    assert_eq!(result.rows_affected(), 1);
+                    info!("Admin user created and set role to supabase_admin");
+        
+                    Ok(gotrue_admin)
+                },
+            }
+        },
+    }
+}
+  
+async fn get_redis_client(redis_uri: &str) -> Result<redis::aio::ConnectionManager, Error> {
+    info!("Connecting to redis with uri: {}", redis_uri);
+    let manager = redis::Client::open(redis_uri)
+        .context("failed to connect to redis")?
+        .get_connection_manager()
+        .await
+        .context("failed to get the connection manager")?;
+    Ok(manager)
+}
+  
+async fn get_aws_s3_bucket(s3_setting: &S3Setting) -> Result<s3::Bucket, Error> {
+    info!("Connecting to S3 bucket with setting: {:?}", &s3_setting);
+    let region = {
+        match s3_setting.use_minio {
+            true => s3::Region::Custom {
+                region: s3_setting.region.to_owned(),
+                endpoint: s3_setting.minio_url.to_owned(),
+            },
+            false => s3_setting
+                .region
+                .parse::<s3::Region>()
+                .context("failed to parser s3 setting")?,
+        }
+    };
+  
+    let cred = s3::creds::Credentials {
+        access_key: Some(s3_setting.access_key.to_owned()),
+        secret_key: Some(s3_setting.secret_key.expose_secret().to_owned()),
+        security_token: None,
+        session_token: None,
+        expiration: None,
+    };
+  
+    match s3::Bucket::create_with_path_style(
+        &s3_setting.bucket,
+        region.clone(),
+        cred.clone(),
+        s3::BucketConfiguration::default(),
+    )
+    .await
+    {
+        Ok(_) => Ok(()),
+        Err(e) => match e {
+            s3::error::S3Error::Http(409, _) => Ok(()), // Bucket already exists
+            _ => Err(e),
+        },
+    }?;
+    Ok(s3::Bucket::new(&s3_setting.bucket, region.clone(), cred.clone())?.with_path_style())
+}
+  
+async fn get_connection_pool(setting: &DatabaseSetting) -> Result<PgPool, Error> {
+    info!(
+        "Connecting to postgres database with setting: {:?}",
+        setting
+    );
+    PgPoolOptions::new()
+        .max_connections(setting.max_connections)
+        .acquire_timeout(Duration::from_secs(10))
+        .max_lifetime(Duration::from_secs(60 * 60))
+        .idle_timeout(Duration::from_secs(60))
+        .connect_with(setting.with_db())
+        .await
+        .map_err(|e| anyhow::anyhow!("Failed to connect to postgres database: {}", e))
+}
+  
+async fn get_gotrue_client(setting: &GoTrueSetting) -> Result<gotrue::api::Client, Error> {
+    info!("Connecting to GoTrue with setting: {:?}", setting);
+    let gotrue_client = gotrue::api::Client::new(reqwest::Client::new(), &setting.base_url);
+    let _ = gotrue_client
+        .health()
+        .await
+        .map_err(|e| anyhow::anyhow!("Failed to connect to GoTrue: {}", e));
+    Ok(gotrue_client)
+}
+  
+fn make_ssl_acceptor_builder(certificate: Secret<String>) -> SslAcceptorBuilder {
+    let mut builder = SslAcceptor::mozilla_intermediate(SslMethod::tls()).unwrap();
+    let x509_cert = X509::from_pem(certificate.expose_secret().as_bytes()).unwrap();
+    builder.set_certificate(&x509_cert).unwrap();
+    builder
+        .set_private_key_file("./cert/key.pem", SslFiletype::PEM)
+        .unwrap();
+    builder
+        .set_certificate_chain_file("./cert/cert.pem")
+        .unwrap();
+    builder
+        .set_min_proto_version(Some(openssl::ssl::SslVersion::TLS1_2))
+        .unwrap();
+    builder
+        .set_max_proto_version(Some(openssl::ssl::SslVersion::TLS1_3))
+        .unwrap();
+    builder
+}
